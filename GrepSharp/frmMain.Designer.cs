62efcbe0a62b3f49839e37e70f08d66351643c01
Felix Houghton <37249527+Fhoughton@users.noreply.github.com>
2018-11-27 20:34:39 +0000

    Add files via upload

---
+++ GrepSharp/frmMain.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace GrepSharp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cboxCaseSensitive = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnBrowseDirectory = new System.Windows.Forms.Button();
+            this.tboxDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.cboxCaseSensitive);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnBrowseDirectory);
+            this.panel1.Controls.Add(this.tboxDirectory);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(6, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(549, 152);
+            this.panel1.TabIndex = 0;
+            // 
+            // cboxCaseSensitive
+            // 
+            this.cboxCaseSensitive.AutoSize = true;
+            this.cboxCaseSensitive.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cboxCaseSensitive.Location = new System.Drawing.Point(10, 121);
+            this.cboxCaseSensitive.Name = "cboxCaseSensitive";
+            this.cboxCaseSensitive.Size = new System.Drawing.Size(136, 17);
+            this.cboxCaseSensitive.TabIndex = 8;
+            this.cboxCaseSensitive.Text = "Search Case-Sensitive:";
+            this.cboxCaseSensitive.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(374, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(374, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Search Regex: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Search Text: ";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Location = new System.Drawing.Point(6, 26);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(71, 71);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Search Type: ";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(4, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Regex";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(4, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Text";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowseDirectory
+            // 
+            this.btnBrowseDirectory.Location = new System.Drawing.Point(514, 6);
+            this.btnBrowseDirectory.Name = "btnBrowseDirectory";
+            this.btnBrowseDirectory.Size = new System.Drawing.Size(28, 20);
+            this.btnBrowseDirectory.TabIndex = 2;
+            this.btnBrowseDirectory.Text = "...";
+            this.btnBrowseDirectory.UseVisualStyleBackColor = true;
+            this.btnBrowseDirectory.Click += new System.EventHandler(this.btnBrowseDirectory_Click);
+            // 
+            // tboxDirectory
+            // 
+            this.tboxDirectory.Location = new System.Drawing.Point(55, 6);
+            this.tboxDirectory.Name = "tboxDirectory";
+            this.tboxDirectory.Size = new System.Drawing.Size(453, 20);
+            this.tboxDirectory.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory: ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(520, 469);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(35, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "About";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(6, 163);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(549, 98);
+            this.panel3.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(8, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Search Subfolders";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.textBox3);
+            this.panel4.Controls.Add(this.comboBox1);
+            this.panel4.Controls.Add(this.radioButton4);
+            this.panel4.Controls.Add(this.radioButton3);
+            this.panel4.Location = new System.Drawing.Point(4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(305, 55);
+            this.panel4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "bytes";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(193, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(79, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "1024";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Greater Than",
+            "Less Than",
+            "Equal To"});
+            this.comboBox1.Location = new System.Drawing.Point(65, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(4, 27);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Size is";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(4, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "All sizes";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(6, 268);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(549, 198);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 147;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Size (Bytes)";
+            this.columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Matches";
+            this.columnHeader3.Width = 59;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Path";
+            this.columnHeader4.Width = 269;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GrepSharp";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tboxDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowseDirectory;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox cboxCaseSensitive;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ GrepSharp/frmMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrepSharp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            tboxDirectory.Text = Directory.GetCurrentDirectory();
+            comboBox1.SelectedIndex = 2;
+        }
+
+        private void btnBrowseDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tboxDirectory.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/Fhoughton");
+        }
+
+        private void addFile(string name,string size,string matches, string path)
+        {
+            ListViewItem item1 = new ListViewItem(name);
+            item1.SubItems.Add(size);
+            item1.SubItems.Add(matches);
+            item1.SubItems.Add(path);
+
+            listView1.Items.AddRange(new ListViewItem[] { item1 });
+        }
+
+        private long getSize(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            return fi.Length;
+        }
+
+        private bool checkText(string path, string text)
+        {
+            string contents = File.ReadAllText(path);
+            return contents.Contains(text);
+        }
+
+        private bool checkTextInsensitive(string path, string text)
+        {
+            string contents = File.ReadAllText(path);
+            return Regex.IsMatch(contents, text, RegexOptions.IgnoreCase);
+        }
+
+        private bool checkRegex(string path, string regex)
+        {
+            string contents = File.ReadAllText(path);
+            return Regex.IsMatch(contents, regex);
+        }
+
+        public static List<string>DirectorySearchAll(string dir)
+        {
+            List<string> files = new List<string>();
+
+            foreach (string f in Directory.GetFiles(dir))
+            {
+                files.Add(Path.GetFullPath(f));
+            }
+            foreach (string d in Directory.GetDirectories(dir))
+            {
+                foreach (string f in Directory.GetFiles(d))
+                {
+                    files.Add(Path.GetFullPath(f));
+                }
+                DirectorySearchAll(d);
+            }
+
+            return files;
+        }
+
+        public static List<string> DirectorySearch(string dir)
+        {
+            List<string> files = new List<string>();
+
+            foreach (string f in Directory.GetFiles(dir))
+            {
+                files.Add(Path.GetFullPath(f));
+            }
+
+            return files;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            List<string> found = new List<string>();
+            List<string> remove = new List<string>();
+            List<int> size = new List<int>();
+            if (checkBox1.Checked == false) { foreach (string f in DirectorySearch(tboxDirectory.Text)) { found.Add(f); } }
+            if (checkBox1.Checked == true) { foreach (string f in DirectorySearchAll(tboxDirectory.Text)) { found.Add(f); } }
+
+            if (radioButton1.Checked)
+            {
+                if (cboxCaseSensitive.Checked)
+                {
+                    foreach (string f in found)
+                    {
+                        if (!checkText(f, textBox1.Text)) { remove.Add(f); }
+                    }
+                }
+                else
+                {
+                    foreach (string f in found)
+                    {
+                        if (!checkTextInsensitive(f, textBox1.Text)) { remove.Add(f); }
+                    }
+                }
+            }
+
+            if (radioButton2.Checked)
+            {
+                foreach (string f in found)
+                {
+                    if (!checkRegex(f, textBox2.Text)) { remove.Add(f); }
+                }
+            }
+
+            if (radioButton4.Checked)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    foreach (string f in found)
+                    {
+                        FileInfo fi = new FileInfo(f);
+                        if (fi.Length < int.Parse(textBox3.Text)) { remove.Add(f); };
+                    }
+                }
+
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    foreach (string f in found)
+                    {
+                            FileInfo fi = new FileInfo(f);
+                            if (fi.Length > int.Parse(textBox3.Text)) { remove.Add(f); };
+                    }
+                }
+
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    foreach (string f in found)
+                    {
+                        FileInfo fi = new FileInfo(f);
+                        if (fi.Length != int.Parse(textBox3.Text)) { remove.Add(f); };
+                    }
+                }
+            }
+
+            foreach (string f in remove)
+            {
+                found.RemoveAt(found.IndexOf(f));
+            }
+
+            foreach (string f in found)
+            {
+                addFile(Path.GetFileName(f), getSize(f).ToString(),"11",f);
+            }
+
+        }
+    }
+}
